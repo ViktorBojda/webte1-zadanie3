8ca27ff2ea4b2515ad395f68e6ba9ca203670b99
Viktor Bojda <viktorbojda17@gmail.com>
2022-11-12 12:05:38 +0100

    Create basic website shape with bootstrap and read xml with jquery

---
+++ js/script.js
@@ -0,0 +1,77 @@
+$(window).on('load', function() {
+    $.ajax({
+        url: 'z03.xml',
+        dataType: 'xml',
+        success: function(xmlDoc) {
+            renderGraphFromXml(xmlDoc);
+        },
+        error: function() {
+            console.log('Failed to load xml data.');
+        }
+    });
+});
+
+function renderGraphFromXml(xmlDoc) {
+    let $xml = $(xmlDoc);
+    $xml.find('zaznam').each(function() {
+        $(this).children().each(function() {
+            console.log(this.textContent);
+        });
+    });
+
+    var options = {
+        series: [{
+        name: 'Net Profit',
+        data: [44, 55, 57, 56, 61, 58, 63, 60, 66]
+      }, {
+        name: 'Revenue',
+        data: [76, 85, 101, 98, 87, 105, 91, 114, 94]
+      }, {
+        name: 'Free Cash Flow',
+        data: [35, 41, 36, 26, 45, 48, 52, 53, 41]
+      }],
+        chart: {
+        type: 'bar',
+        height: 350
+      },
+      plotOptions: {
+        bar: {
+          horizontal: false,
+          columnWidth: '55%',
+          endingShape: 'rounded'
+        },
+      },
+      dataLabels: {
+        enabled: false
+      },
+      stroke: {
+        show: true,
+        width: 2,
+        colors: ['transparent']
+      },
+      xaxis: {
+        categories: ['Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct'],
+      },
+      yaxis: {
+        title: {
+          text: '$ (thousands)'
+        }
+      },
+      fill: {
+        opacity: 1
+      },
+      tooltip: {
+        y: {
+          formatter: function (val) {
+            return "$ " + val + " thousands"
+          }
+        }
+      }
+      };
+
+      var chart = new ApexCharts(document.querySelector("#chart"), options);
+      chart.render();
+
+}
+
+
